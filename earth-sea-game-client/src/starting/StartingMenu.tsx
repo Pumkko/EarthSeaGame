cdea2f40d5cf86dd8365b0ccf94bb0550053d638
pumkko <c.coque@hotmail.fr>
2024-03-11 15:27:41 -0400

    add shadcn (solid port) add sass, add starting menu

--- earth-sea-game-client/src/starting/StartingMenu.tsx
+++ earth-sea-game-client/src/starting/StartingMenu.tsx
@@ -1,8 +1,13 @@
-import './StartingMenu.css'
+import { Button } from '~/components/ui/button'
+import './StartingMenu.scss'
 
 export default function StartingMenu() {
-    return <div class="container">
-
+    return <div class="starting-menu-container">
+        <p class='game-title'>Between Earth and Sea</p>
+        <div class='starting-menu-buttons'>
+            <Button variant={'secondary'}>Start New Game</Button>
+            <Button variant={'secondary'}>Join Game</Button>
+        </div>
     </div>
 
 }
