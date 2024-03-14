03ce8d50d534dbd6cf2df57e806c2292258352db
pumkko <c.coque@hotmail.fr>
2024-03-14 10:26:31 -0400

    add path for lib folder

--- earth-sea-game-client/src/lib/Environment.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const EnvironmentSchema = z.object({
-  VITE_API_ROOT_URL: z.string().url(),
-});
--- earth-sea-game-client/src/schemas/GameLobbySchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const GameLobbySchema = z.object({
-  id: z.string().uuid(),
-  gameMaster: z.string(),
-  lobbyName: z.string(),
-});
-
-export type GameLobby = z.infer<typeof GameLobbySchema>;
