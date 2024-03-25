78a841f1352663046346615d67ff0f0a18e6deaf
pumkko <c.coque@hotmail.fr>
2024-03-25 16:19:36 -0400

    use form to send messages

--- earth-sea-game-client/src/components/Chat.tsx
+++ earth-sea-game-client/src/components/Chat.tsx
@@ -1,6 +1,7 @@
 import { ChatMessageModel, ChatMessageSender } from "@lib/schemas/MessageSchema";
-import { For, createSignal } from "solid-js";
+import { For } from "solid-js";
 import ChatMessage from "./ChatMessage";
+import { createForm } from "@tanstack/solid-form";
 
 interface ChatProps {
     currentUser: ChatMessageSender;
@@ -11,8 +12,6 @@ interface ChatProps {
 }
 
 export default function Chat(props: ChatProps) {
-    const [newMessage, setNewMessage] = createSignal("");
-
     const beautifulNationName = () => {
         switch (props.recipient) {
             case "EarthNation":
@@ -26,28 +25,49 @@ export default function Chat(props: ChatProps) {
         }
     };
 
+    const form = createForm(() => ({
+        defaultValues: {
+            message: "",
+        },
+        onSubmit: async ({ value, formApi }) => {
+            await props.onNewMessage(value.message);
+            formApi.setFieldValue("message", "");
+        },
+    }));
+
     return (
         <div class={`flex flex-col overflow-auto ${props.isMiddleChat ? "border-x-2" : ""}`}>
             <h1 class="self-center">{beautifulNationName()}</h1>
-            <div class="flex flex-col flex-grow justify-end">
+            <div class="flex flex-col flex-grow justify-end p">
                 <For each={props.messages}>{(message) => <ChatMessage message={message} currentUser={props.currentUser} />}</For>
             </div>
-            <div class="flex">
-                <input
-                    class="text-black border-2 rounded border-black m-2 p-2 flex-grow"
-                    value={newMessage()}
-                    onChange={(e) => setNewMessage(e.currentTarget.value)}
-                />
-                <button
-                    class="border-2 px-2 mr-2 w-20"
-                    onClick={async () => {
-                        await props.onNewMessage(newMessage());
-                        setNewMessage("");
+            <form.Provider>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        void form.handleSubmit();
                     }}
                 >
-                    Send
-                </button>
-            </div>
+                    <div>
+                        <form.Field
+                            name="message"
+                            children={(field) => (
+                                <input
+                                    class="text-black w-full border-2 rounded border-black m-2 p-2 flex-grow"
+                                    name={field().name}
+                                    value={field().state.value}
+                                    onBlur={field().handleBlur}
+                                    onInput={(e) => field().handleChange(e.target.value)}
+                                />
+                            )}
+                        />
+                    </div>
+                    <button class="border-2 px-2 mr-2 w-20" type="submit">
+                        Submit
+                    </button>
+                </form>
+            </form.Provider>
         </div>
     );
 }
