03bcbb26051dbda006291447bf48f6cb9a3f592f
pumkko <c.coque@hotmail.fr>
2024-04-01 14:36:17 -0400

    reorder timeline i18n so it fits the chronology

--- earth-sea-game-client/src/i18n/fr/earthNationTimeline.fr.ts
+++ earth-sea-game-client/src/i18n/fr/earthNationTimeline.fr.ts
@@ -11,79 +11,79 @@ export const EarthNationTimelineFr: Language["timelines"]["earthNation"] = {
         details:
             "La situation sur le continent s'embrase et le gouvernement est massacré. Les plus puissantes factions du continent s'arment, le territoire plonge dans la violence.",
     },
-    atomicBombings: {
-        event: "Bombardements Atomiques de la Nation Reculée",
+    seaNationProtectionTreaty: {
+        event: "Traité de Protection",
         details:
-            "Acculée, reculant sur tous les fronts sans aucun espoit de victoire, la Nation Reculée continue malgré tout de combattre. La Nation Des Mers décide alors d'utiliser une nouvelle arme pour mettre fin à la guerre.",
+            "Face au chaos du continent, l'Île Orientale auparavant une province de la Nation de la Terre, demande la protection de la Nation Des Mers.",
     },
-    cohabitationTreaty: {
-        event: "Traité de Cohabitation",
+    warlordEraBegins: {
+        event: "Âge des Seigneurs de Guerre",
         details:
-            "le Traité de Cohabitation entre la Nation de la Terre et la Nation du Sud est signé. Celui-ci met fin à la guerre et à travers l'article 4 du traité chaque nation reconnait une différence de point de vue non résolu sur le futur du continent.",
-    },
-    firstNuclearReactor: {
-        event: "Mise en service du R1",
-        details:
-            "Le R1 premier réacteur nucléaire construit par la Nation de la Terre entre en service dans le plus grand secret.",
-    },
-    firstStraitCrisis: {
-        event: "Première crise du détroit",
-        details:
-            "Le gourvernement de la Renaissance, désireux de rétablir les frontières de l'ancienne Nation de la Terre, lance une expédition sur l'Île Orientale. La Nation Des Mers liée par traité réagit en envoyant deux porte-avions et leur escorte. La Nation de la Terre largement dépassée doit se retirer.",
+            "La guerre civile perd en intensité, la situation se stabilise, le territoire de l'ancienne Nation de la Terre est fragmenté et contrôlé par une multitude de seigneurs de guerre à la puissance variable.",
     },
     greatWarBegins: {
         event: "Début de la Grande Guerre",
         details:
             "La Nation Reculée, souhaitant profiter du chaos sur le continent pour en prendre le contrôle lance une attaque massive tous azimut, la Grande Guerre commence.",
     },
+    atomicBombings: {
+        event: "Bombardements Atomiques de la Nation Reculée",
+        details:
+            "Acculée, reculant sur tous les fronts sans aucun espoit de victoire, la Nation Reculée continue malgré tout de combattre. La Nation Des Mers décide alors d'utiliser une nouvelle arme pour mettre fin à la guerre.",
+    },
     greatWarEnds: {
         event: "Fin de la Grande Guerre",
         details:
             "La Nation Reculée capitule après avoir vu ses trois plus grandes villes détruites par des bombardements atomiques. La Grande Guerre est terminée, plus de 80 millions de personnes y ont perdu la vie. Le Continent principal lieu des combats est ravagée, la population décimée. La Nation Des Mers devient de très loin l'état le plus puissant du monde et seul doté de l'arme nucléaire.",
-    },
-    rebirthOfTheNation: {
-        event: "Proclamation de la Renaissance de la Nation",
-        details:
-            "Une Faction sort victorieuse de la deuxième guerre civile. Ses chefs proclament la Renaissance de la Nation de la Terre.",
-    },
-    seaNationProtectionTreaty: {
-        event: "Traité de Protection",
-        details:
-            "Face au chaos du continent, l'Île Orientale auparavant une province de la Nation de la Terre, demande la protection de la Nation Des Mers.",
     },
     secondCivilWarBegins: {
         event: "Début de la Deuxième Guerre Civile",
         details:
             "La Grande Guerre a laissé le continent ravagé, les Seigneurs de guerre qui quelques années auparavant garantissaient une paix fragile sont décimés.",
     },
-    secondStraitCrisis: {
-        event: "Deuxième crise du détroit",
+    rebirthOfTheNation: {
+        event: "Proclamation de la Renaissance de la Nation",
         details:
-            "Désormais supportée par la Nation du Nord, le gouvernement de la Renaissance tente une nouvelle fois de ramener l'Île Orientale dans le territoire de la Nation de la Terre. La Nation Des Mers réagit avec trois porte-avions et ordonne le déploiement préventif de ses forces nucléaires.",
+            "Une Faction sort victorieuse de la deuxième guerre civile. Ses chefs proclament la Renaissance de la Nation de la Terre.",
     },
-    sinkingOfTheNepture: {
-        event: "Naufrage du Nepture",
+    firstStraitCrisis: {
+        event: "Première crise du détroit",
         details:
-            "Le Neptune, navire de croisière battant le pavillon de la Nation Des Mers sombre à quelques milles nautiques de l'Île Orientale.",
+            "Le gourvernement de la Renaissance, désireux de rétablir les frontières de l'ancienne Nation de la Terre, lance une expédition sur l'Île Orientale. La Nation Des Mers liée par traité réagit en envoyant deux porte-avions et leur escorte. La Nation de la Terre largement dépassée doit se retirer.",
     },
     southerNationBorderConflict: {
         event: "Guerre contre la Nation du Sud",
         details:
             "Toujours dans le but de rétablir l'héritage de l'ancienne Nation de la Terre, le nouveau gouvernement lance une expédition vers le sud. Ce territoire contrôlé par l'un des derniers seigneurs de guerre résiste, les généraux du Sud maneouvrent habilement et tiennent bon face aux assaults de la Nation de la Terre.",
     },
-    southernNationIndependence: {
-        event: "Indépendance de la Nation du Sud",
-        details:
-            "Considérant le traité de cohabitation comme une victoire, la Nation du Sud déclare officiellement son indépendance. Pour son premier discours le chef de l'état de la nouvelle Nation exprime le besoin pour le continent de faire le deuil de l'ancienne Nation de la Terre.",
-    },
     treatyOfUnity: {
         event: "Traité d'Unité",
         details:
             "Humiliée par sa défaite lors de sa tentative d'envahir l'Île Orientale la Nation de la Terre signe un traité d'amitié avec la Nation du Nord. La Nation du Nord devient dans les faits un état satellite de la Nation de la Terre.",
     },
-    warlordEraBegins: {
-        event: "Âge des Seigneurs de Guerre",
+    cohabitationTreaty: {
+        event: "Traité de Cohabitation",
         details:
-            "La guerre civile perd en intensité, la situation se stabilise, le territoire de l'ancienne Nation de la Terre est fragmenté et contrôlé par une multitude de seigneurs de guerre à la puissance variable.",
+            "le Traité de Cohabitation entre la Nation de la Terre et la Nation du Sud est signé. Celui-ci met fin à la guerre et à travers l'article 4 du traité chaque nation reconnait une différence de point de vue non résolu sur le futur du continent.",
+    },
+    southernNationIndependence: {
+        event: "Indépendance de la Nation du Sud",
+        details:
+            "Considérant le traité de cohabitation comme une victoire, la Nation du Sud déclare officiellement son indépendance. Pour son premier discours le chef de l'état de la nouvelle Nation exprime le besoin pour le continent de faire le deuil de l'ancienne Nation de la Terre.",
+    },
+    secondStraitCrisis: {
+        event: "Deuxième crise du détroit",
+        details:
+            "Désormais supportée par la Nation du Nord, le gouvernement de la Renaissance tente une nouvelle fois de ramener l'Île Orientale dans le territoire de la Nation de la Terre. La Nation Des Mers réagit avec trois porte-avions et ordonne le déploiement préventif de ses forces nucléaires.",
+    },
+    firstNuclearReactor: {
+        event: "Mise en service du R1",
+        details:
+            "Le R1 premier réacteur nucléaire construit par la Nation de la Terre entre en service dans le plus grand secret.",
+    },
+    sinkingOfTheNepture: {
+        event: "Naufrage du Nepture",
+        details:
+            "Le Neptune, navire de croisière battant le pavillon de la Nation Des Mers sombre à quelques milles nautiques de l'Île Orientale.",
     },
 };
--- earth-sea-game-client/src/i18n/fr/easternIslandTimeline.fr.ts
+++ earth-sea-game-client/src/i18n/fr/easternIslandTimeline.fr.ts
@@ -11,74 +11,75 @@ export const EasternNationTimeline: Language["timelines"]["easternIsland"] = {
         details:
             "La situation sur le continent s'embrase et le gouvernement est massacré. Les plus puissantes factions du continent s'arment, le territoire plonge dans la violence.",
     },
-    atomicBombings: {
-        event: "Bombardements Atomiques de la Nation Reculée",
+    seaNationProtectionTreaty: {
+        event: "Traité de Protection",
         details:
-            "Acculée, reculant sur tous les fronts sans aucun espoit de victoire, la Nation Reculée continue malgré tout de combattre. La Nation Des Mers décide alors d'utiliser une nouvelle arme pour mettre fin à la guerre.",
+            "Face au chaos du continent, l'Île Orientale auparavant une province de la Nation de la Terre, demande la protection de la Nation Des Mers.",
     },
-    cohabitationTreaty: {
-        event: "Traité de Cohabitation",
+    warlordEraBegins: {
+        event: "Âge des Seigneurs de Guerre",
         details:
-            "le Traité de Cohabitation entre la Nation de la Terre et la Nation du Sud est signé. Celui-ci met fin à la guerre et à travers l'article 4 du traité chaque nation reconnait une différence de point de vue non résolu sur le futur du continent.",
-    },
-    firstStraitCrisis: {
-        event: "Première crise du détroit",
-        details:
-            "Le gouvernement de la Renaissance décide d'unifier par la force les anciens territoires de la Nation de la Terre. Plusieurs armées sont rassemblées sur le continent, la Nation Des Mers liée par traité réagit en déployant deux porte-avions et leur escorte dans le détroit. La Nation de la Terre largement dépassée doit retirer ses troupes.",
+            "La guerre civile perd en intensité, la situation se stabilise, le territoire de l'ancienne Nation de la Terre est fragmenté et contrôlé par une multitude de seigneurs de guerre à la puissance variable.",
     },
     greatWarBegins: {
         event: "Début de la Grande Guerre",
         details:
             "La Nation Reculée, souhaitant profiter du chaos sur le continent pour en prendre le contrôle lance une attaque massive tous azimut, la Grande Guerre commence.",
     },
+    atomicBombings: {
+        event: "Bombardements Atomiques de la Nation Reculée",
+        details:
+            "Acculée, reculant sur tous les fronts sans aucun espoit de victoire, la Nation Reculée continue malgré tout de combattre. La Nation Des Mers décide alors d'utiliser une nouvelle arme pour mettre fin à la guerre.",
+    },
     greatWarEnds: {
         event: "Fin de la Grande Guerre",
         details:
             "La Nation Reculée capitule après avoir vu ses trois plus grandes villes détruites par des bombardements atomiques. La Grande Guerre est terminée, plus de 80 millions de personnes y ont perdu la vie. Le Continent est ravagée, la Nation Des Mers devient de très loin l'état le plus puissant du monde et seul doté de l'arme nucléaire.",
-    },
-    rebirthOfTheNation: {
-        event: "Proclamation de la Renaissance de la Nation",
-        details:
-            "Une Faction sort victorieuse de la deuxième guerre civile. Ses chefs proclament la Renaissance de la Nation de la Terre. L'annonce est accueilli avec inquiétude par les territoires du Sud et l'Île Orientale.",
-    },
-    seaNationProtectionTreaty: {
-        event: "Traité de Protection",
-        details:
-            "Face au chaos du continent, l'Île Orientale auparavant une province de la Nation de la Terre, demande la protection de la Nation Des Mers.",
     },
     secondCivilWarBegins: {
         event: "Début de la Deuxième Guerre Civile",
         details:
             "La Grande Guerre a laissé le continent ravagé, les Seigneurs de guerre qui quelques années auparavant garantissaient une paix fragile sont décimés.",
     },
-    secondStraitCrisis: {
-        event: "Deuxième crise du détroit",
+    rebirthOfTheNation: {
+        event: "Proclamation de la Renaissance de la Nation",
         details:
-            "Désormais supportée par la Nation du Nord, le gouvernement de la Renaissance tente une nouvelle fois de ramener l'Île Orientale dans le territoire de la Nation de la Terre. La Nation Des Mers réagit avec trois porte-avions et ordonne le déploiement préventif de ses forces nucléaires.",
+            "Une Faction sort victorieuse de la deuxième guerre civile. Ses chefs proclament la Renaissance de la Nation de la Terre. L'annonce est accueilli avec inquiétude par les territoires du Sud et l'Île Orientale.",
     },
-    sinkingOfTheNepture: {
-        event: "Naufrage du Nepture",
+
+    firstStraitCrisis: {
+        event: "Première crise du détroit",
         details:
-            "Le Neptune, navire de croisière battant le pavillon de la Nation Des Mers sombre à quelques milles nautiques de l'Île Orientale.",
+            "Le gouvernement de la Renaissance décide d'unifier par la force les anciens territoires de la Nation de la Terre. Plusieurs armées sont rassemblées sur le continent, la Nation Des Mers liée par traité réagit en déployant deux porte-avions et leur escorte dans le détroit. La Nation de la Terre largement dépassée doit retirer ses troupes.",
     },
     southerNationBorderConflict: {
         event: "Guerre contre la Nation du Sud",
         details:
             "Après l'échec de sa tentive de contrôler l'Île Orientale le gouvernemnet de la Renaissance tente d'envahir son voisin du Sud. Les généraux du Sud maneouvrent habilement et tiennent bon face aux assaults de la Nation de la Terre.",
     },
-    southernNationIndependence: {
-        event: "Indépendance de la Nation du Sud",
-        details:
-            "Considérant le traité de cohabitation comme une victoire, la Nation du Sud déclare officiellement son indépendance. Pour son premier discours le chef de l'état de la nouvelle Nation exprime le besoin pour le continent de faire le deuil de l'ancienne Nation de la Terre.",
-    },
     treatyOfUnity: {
         event: "Traité d'Unité",
         details:
             "Humiliée par sa défaite lors de sa tentative d'envahir l'Île Orientale la Nation de la Terre signe un traité d'amitié avec la Nation du Nord. La Nation du Nord devient dans les faits un état satellite de la Nation de la Terre.",
     },
-    warlordEraBegins: {
-        event: "Âge des Seigneurs de Guerre",
+    cohabitationTreaty: {
+        event: "Traité de Cohabitation",
         details:
-            "La guerre civile perd en intensité, la situation se stabilise, le territoire de l'ancienne Nation de la Terre est fragmenté et contrôlé par une multitude de seigneurs de guerre à la puissance variable.",
+            "le Traité de Cohabitation entre la Nation de la Terre et la Nation du Sud est signé. Celui-ci met fin à la guerre et à travers l'article 4 du traité chaque nation reconnait une différence de point de vue non résolu sur le futur du continent.",
+    },
+    southernNationIndependence: {
+        event: "Indépendance de la Nation du Sud",
+        details:
+            "Considérant le traité de cohabitation comme une victoire, la Nation du Sud déclare officiellement son indépendance. Pour son premier discours le chef de l'état de la nouvelle Nation exprime le besoin pour le continent de faire le deuil de l'ancienne Nation de la Terre.",
+    },
+    secondStraitCrisis: {
+        event: "Deuxième crise du détroit",
+        details:
+            "Désormais supportée par la Nation du Nord, le gouvernement de la Renaissance tente une nouvelle fois d'envahir l'Île Orientale. La Nation Des Mers réagit avec trois porte-avions et ordonne le déploiement préventif de ses forces nucléaires.",
+    },
+    sinkingOfTheNepture: {
+        event: "Naufrage du Nepture",
+        details:
+            "Le Neptune, navire de croisière battant le pavillon de la Nation Des Mers explose à quelques milles nautiques de l'Île Orientale.",
     },
 };
