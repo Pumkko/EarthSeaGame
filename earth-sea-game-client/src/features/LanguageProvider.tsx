9bcefab43fd521312b6dffc1647ee7954f5e9621
pumkko <c.coque@hotmail.fr>
2024-04-02 13:55:17 -0400

    add favico localize title

--- earth-sea-game-client/src/features/LanguageProvider.tsx
+++ earth-sea-game-client/src/features/LanguageProvider.tsx
@@ -1,4 +1,4 @@
-import { Accessor, JSXElement, createContext, createMemo, useContext } from "solid-js";
+import { Accessor, JSXElement, createContext, createEffect, createMemo, useContext } from "solid-js";
 import { fr } from "../i18n/fr/fr";
 
 // I can get away with typeof fr because fr is typed through Language so are every language definition
@@ -6,7 +6,13 @@ import { fr } from "../i18n/fr/fr";
 const LanguageContext = createContext<Accessor<typeof fr>>();
 
 export function LanguageProvider(props: { children: JSXElement }) {
-    const dict = createMemo(() => fr);
+    const dict = createMemo(() => {
+        return fr;
+    });
+
+    createEffect(() => {
+        document.title = fr.gameTitle();
+    });
 
     return <LanguageContext.Provider value={dict}>{props.children}</LanguageContext.Provider>;
 }
